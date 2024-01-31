70c4e1405a56f1ab1478525a1c98376777753941
soft1c <softicahmedd@gmail.com>
2024-01-31 02:57:01 +0100

    footer

--- baza.js
+++ baza.js
@@ -6,52 +6,12 @@ let db = new sqlite3.Database('./baza.db', (err) => {
         console.error(err.message);
         throw err;
     } else {
-        let query3= `DROP TABLE IF EXISTS shop`;
-        db.run(query3,(err)=>{
-            if(err){
-                console.log(err);
-            }else{
-                console.log('ok');
-            }
-        })
-        let query=`CREATE TABLE IF NOT EXISTS shop (
-            id INTEGER PRIMARY KEY,
-            naziv TEXT,
-            cijena REAL,
-            slika TEXT,
-            opis TEXT
-        )`;
-
-        db.run(query, (err)=>{
-            if(err){
-                console.log(err);
-            }else{
-                console.log('ok');
-            }
-        });
-        
-        let query2= `CREATE TABLE   IF NOT EXISTS donate(
-            id INTEGER PRIMARY KEY,
-            link TEXT,
-            qr TEXT
-        )`;
-        
-        db.run(query2, (req,res)=>{
-            if(err){
-                console.log(err);
-            }else{
-                console.log('ok');
-            }
-        })
-    }
-  }
-    
-);
-
-db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Connection to the SQLite database closed.');
-    }
-});
+        let query='INSERT INTO footer (urlPath,tekst) VALUES ("uploads/media/images/logo2.png-1706398342231-361408291.png","Footer")';
+        db.run(query, (err) => {
+            if (err) {
+                console.error(err.message);
+                throw err;
+            } else {
+                console.log('Footer inserted');
+    }});
+    }});
